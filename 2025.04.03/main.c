564e7eb580f75fb9d5633d559cf3b3442ad61877
KIM JIN BEOM <180479799+jinbeom0325@users.noreply.github.com>
2025-04-09 10:15:31 +0900

    Update main.c

--- 2025.04.03/main.c
+++ 2025.04.03/main.c
@@ -11,18 +11,18 @@
 
 int main(void)
 {
-	uint32_t *pClkCtrlReg =   (uint32_t*)0x40023830;
-	uint32_t *pPortDModeReg = (uint32_t*)0x40020C00;
-	uint32_t *pPortDOutReg =  (uint32_t*)0x40020C14;
+	uint32_t *pClkCtrlReg =   (uint32_t*)0x40023830; //RCC
+	uint32_t *pPortDModeReg = (uint32_t*)0x40020C00; //GPIOD MODER
+	uint32_t *pPortDOutReg =  (uint32_t*)0x40020C14; //GPIOD ODR
 
 	//1. enable the clock for GPOID peripheral in the AHB1ENR (SET the 3rd bit position)
 	*pClkCtrlReg |= ( 1 << 3);
 
 	//2. configure the mode of the IO pin as output
 	//a. clear the 24th and 25th bit positions (CLEAR)
-	*pPortDModeReg &= ~( 3 << 24);
+	*pPortDModeReg &= ~( 3 << 24); //초기화 12번핀 
 	//b. make 24th bit position as 1 (SET)
-	*pPortDModeReg |= ( 1 << 24);
+	*pPortDModeReg |= ( 1 << 24); //01
 
 	//3.SET 12th bit of the output data register to make I/O pin-12 as HIGH
 	*pPortDOutReg |= ( 1 << 12);
